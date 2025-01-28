de551edb546024ce42e95087b2405cbc410fda44
Neshyna <mariya.neshyna@gmail.com>
2025-01-28 02:51:16 +0100

    dis true

--- src/hw/hw8/HW8.tsx
+++ src/hw/hw8/HW8.tsx
@@ -1,10 +1,10 @@
 import { ButtonProps } from "./types.ts";
 import { MainButton } from "./styles.ts";
 
-function Button({ name = "SEND", type, onClick }: ButtonProps) {
+function Button({ name = "SEND", type, onClick, disabled}: ButtonProps) {
   return (
     <div>
-      <MainButton disabled type={type} onClick={onClick}>
+      <MainButton disabled={true} type={type} onClick={onClick}>
         {name}
       </MainButton>
       <MainButton type={type} onClick={onClick}>
